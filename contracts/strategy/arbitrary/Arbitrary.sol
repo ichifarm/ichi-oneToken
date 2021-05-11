27005a38d24742a3cd4bdc6a71e7b32913a136a4
3338e30b4a154caeafffecfe1a301a956c304862 <noreply@nowhere.com>
2021-05-11 16:56:36 -0600

    not payable

--- contracts/strategy/arbitrary/Arbitrary.sol
+++ contracts/strategy/arbitrary/Arbitrary.sol
@@ -13,14 +13,12 @@ contract Arbitrary is StrategyCommon {
 
     /**
     @notice Governance can work with collateral and treasury assets. Can swap assets.
-           Add assets with oracles to include newly acquired tokens in inventory for reporting/accounting functions.
     @param target address/smart contract you are interacting with
     @param value msg.value (amount of eth in WEI you are sending. Most of the time it is 0)
-    @param signature the function signature (name of the function and the types of the arguments)
-           for example: "transfer(address,uint256)", or "approve(address,uint256)"
-    @param data abi-encodeded byte-code of the parameter values you are sending.
+    @param signature the function signature
+    @param data abi-encodeded bytecode of the parameter values to send
     */
-    function executeTransaction(address target, uint value, string memory signature, bytes memory data) public payable onlyOwner returns (bytes memory) {
+    function executeTransaction(address target, uint value, string memory signature, bytes memory data) external onlyOwner returns (bytes memory) {
         bytes memory callData;
 
         if (bytes(signature).length == 0) {
