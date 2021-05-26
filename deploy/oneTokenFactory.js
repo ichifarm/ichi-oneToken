88a2ebd65c2bf8ba749667e822e03ca86280dbe4
37 Aces <75387898+37aces@users.noreply.github.com>
2021-05-26 07:44:01 -0400

    Pre Deployment Updates (#1)
    * deploy scripts for hardhat to testnet
    
    * add deployments
    
    * audit report update

---
+++ deploy/oneTokenFactory.js
@@ -0,0 +1,16 @@
+module.exports = async function({ ethers: { getNamedSigner }, getNamedAccounts, deployments }) {
+    const { deploy } = deployments
+  
+    const { deployer, dev } = await getNamedAccounts()
+  
+    const chainId = await getChainId()
+
+    const factory = await deploy("OneTokenFactory", {
+        from: deployer,
+        log: true,
+        deterministicDeployment: false
+    })
+
+}
+
+module.exports.tags = ["oneTokenFactory","init"]
