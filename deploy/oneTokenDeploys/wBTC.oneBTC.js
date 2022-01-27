fc043b4011cbfdc0a1ccadec27f8c0de8c684e4e
Miguel Palhas <mpalhas@gmail.com>
2022-01-27 15:58:16 +0000

    code review

--- deploy/oneTokenDeploys/wBTC.oneBTC.js
+++ deploy/oneTokenDeploys/wBTC.oneBTC.js
@@ -52,7 +52,7 @@ module.exports = async function({ ethers: { getNamedSigner }, getNamedAccounts, 
             'deployOneTokenProxy',
             name,
             symbol,
-            deployer, // TODO should we set this to governance right away/
+            deployer, // TODO should we set this to governance right away?
             version.address,
             controller.address,
             mintMaster.address,
