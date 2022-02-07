004a7b4623db6add73d98f9421fb87134e797805
Miguel Palhas <mpalhas@gmail.com>
2022-02-07 14:02:49 +0000

    code review

--- scripts/setMumbaiMintingRatio.ts
+++ scripts/setMumbaiMintingRatio.ts
@@ -20,13 +20,14 @@ async function main() {
   const config = getCurrentConfig();
   const [owner] = await ethers.getSigners();
 
-  const ratio45   = "450000000000000000"; // 90%
-  const ratio95   = "950000000000000000"; // 90%
-  const ratio100 = "1000000000000000000"; // 90%
+  const ratio45   = "450000000000000000"; // 45%
+  const ratio80   = "800000000000000000";
+  const ratio95   = "950000000000000000";
+  const ratio100 = "1000000000000000000";
   const maxVol = "9999999999999999999999999999999999999999999999999"; // approximately unlimited
 
   const oneBTC = get('oneBTC');
-  await execute('Incremental', {from: deployer, log: true}, 'setParam', oneBTC.address, ratio95, ratio100, 0, ratio100, maxVol);
+  await execute('Incremental', {from: deployer, log: true}, 'setParam', oneBTC.address, ratio80, ratio100, 0, ratio95, maxVol);
   await execute('oneBTC', {from: deploy, log: true}, 'setRedemptionFee', ratio45)
 }
 
