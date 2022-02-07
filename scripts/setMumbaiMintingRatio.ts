4744684a49d34d37557271a87d79a94f24dfa393
Miguel Palhas <mpalhas@gmail.com>
2022-02-07 14:10:33 +0000

    code review

--- scripts/setMumbaiMintingRatio.ts
+++ scripts/setMumbaiMintingRatio.ts
@@ -15,10 +15,7 @@ const TargetsPerNetwork = {
 }
 
 async function main() {
-  const { get, read, execute } = deployments;
-  const targets = TargetsPerNetwork[network.config.chainId];
-  const config = getCurrentConfig();
-  const [owner] = await ethers.getSigners();
+  const { get } = deployments;
 
   const ratio45   = "450000000000000000"; // 45%
   const ratio80   = "800000000000000000";
@@ -26,9 +23,16 @@ async function main() {
   const ratio100 = "1000000000000000000";
   const maxVol = "9999999999999999999999999999999999999999999999999"; // approximately unlimited
 
-  const oneBTC = get('oneBTC');
-  await execute('Incremental', {from: deployer, log: true}, 'setParam', oneBTC.address, ratio80, ratio100, 0, ratio95, maxVol);
-  await execute('oneBTC', {from: deploy, log: true}, 'setRedemptionFee', ratio45)
+  const deps = {
+    incremental: await get('Incremental'),
+    oneBTC: await get('oneBTC')
+  }
+
+  const incremental = await ethers.getContractAt('Incremental', deps.incremental.address);
+  const oneBTC = await ethers.getContractAt('OneTokenV1', deps.oneBTC.address);
+
+  await incremental.setParams(deps.oneBTC.address, ratio80, ratio100, 0, ratio95, maxVol);
+  await oneBTC.setRedemptionFee(ratio45);
 }
 
 main()
