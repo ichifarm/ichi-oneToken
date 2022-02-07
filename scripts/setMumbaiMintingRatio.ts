7df9992e26e30ea499a64c5b0fe65c836f8b0964
Miguel Palhas <mpalhas@gmail.com>
2022-02-07 11:51:35 +0000

    code review

---
+++ scripts/setMumbaiMintingRatio.ts
@@ -0,0 +1,38 @@
+import { ethers, deployments, network, getNamedAccounts } from "hardhat";
+
+import { getCurrentConfig } from "../src/deployConfigs";
+
+const TargetsPerNetwork = {
+  137: [
+    "Incremental",
+    "OneTokenFactory",
+    "OneTokenV1",
+    "ICHIPeggedOracle",
+    "ChainlinkOracleUSD",
+    "oneBTC",
+    "oneBTCArbitrary"
+  ]
+}
+
+async function main() {
+  const { get, read, execute } = deployments;
+  const targets = TargetsPerNetwork[network.config.chainId];
+  const config = getCurrentConfig();
+  const [owner] = await ethers.getSigners();
+
+  const ratio45   = "450000000000000000"; // 90%
+  const ratio95   = "950000000000000000"; // 90%
+  const ratio100 = "1000000000000000000"; // 90%
+  const maxVol = "9999999999999999999999999999999999999999999999999"; // approximately unlimited
+
+  const oneBTC = get('oneBTC');
+  await execute('Incremental', {from: deployer, log: true}, 'setParam', oneBTC.address, ratio95, ratio100, 0, ratio100, maxVol);
+  await execute('oneBTC', {from: deploy, log: true}, 'setRedemptionFee', ratio45)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error: any) => {
+    console.error(error);
+    process.exit(1);
+  });
